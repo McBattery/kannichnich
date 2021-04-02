36210cc731a1fe2c0de04bf95a5f196776e337b1
Jan Cramer <61551286+McBattery@users.noreply.github.com>
2021-04-02 14:39:02 +0200

    Update gamedemo.js

---
+++ gamedemo.js
@@ -0,0 +1,7 @@
+var leinwand = document.getElementById('leinwand').getContext('2d');
+
+// Mit 'fillStyle' wird die Farbe gesetzt (Farbname)
+leinwand.fillStyle = "Deepskyblue";
+
+// Mit 'fillRect' wird das Rechteck erzeugt
+leinwand.fillRect(200, 100, 1000, 1000);
